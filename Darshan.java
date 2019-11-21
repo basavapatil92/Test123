627643b3711b1ec21acce4df326de55b34276ea8
basavapatil92 <basavapatilqa@gmail.com>
2019-11-21 22:31:05 +0530

    uploading first Jar file

--- Darshan.java
+++ Darshan.java
@@ -3,6 +3,10 @@ class Darshan
 	public static void main(String args[])
 	{
 		System.out.println("What ever your my best friend");
+		for(int i =1;i<5;i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
